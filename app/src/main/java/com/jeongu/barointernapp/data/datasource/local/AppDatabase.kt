450c06f61ac80183af739f385ec84fc579e720fc
seojw0124 <wjddn2000124@gmail.com>
2025-03-16 04:03:52 +0900

    feat: Dto의 TypeConvert 추가

--- app/src/main/java/com/jeongu/barointernapp/data/datasource/local/AppDatabase.kt
+++ app/src/main/java/com/jeongu/barointernapp/data/datasource/local/AppDatabase.kt
@@ -6,6 +6,7 @@ import androidx.room.TypeConverters
 import com.jeongu.barointernapp.data.model.ProductLocal
 
 @Database(entities = [ProductLocal::class], version = RoomConstant.ROOM_VERSION)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun productDao(): ProductDao
